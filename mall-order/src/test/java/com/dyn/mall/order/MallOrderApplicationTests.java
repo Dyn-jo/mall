9e10b2144d118e2814fa0269e4255200774b6e03
Dyn-jo <596324727@qq.com>
2020-12-19 20:47:44 +0800

    项目初始结构创建

---
+++ mall-order/src/test/java/com/dyn/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyn.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
