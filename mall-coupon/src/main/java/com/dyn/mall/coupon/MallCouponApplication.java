4c6a17c4dc5401fa20b55b52bb3b1ec262fa1e09
Dyn-jo <596324727@qq.com>
2020-12-24 21:19:58 +0800

    项目配置 配置文件

--- mall-coupon/src/main/java/com/dyn/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/dyn/mall/coupon/MallCouponApplication.java
@@ -1,9 +1,13 @@
 package com.dyn.mall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.dyn.mall.*.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallCouponApplication {
 
     public static void main(String[] args) {
--- mall-order/src/main/java/com/dyn/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/dyn/mall/order/MallOrderApplication.java
@@ -1,9 +1,13 @@
 package com.dyn.mall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.dyn.mall.*.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallOrderApplication {
 
     public static void main(String[] args) {
